1e3bef61036aa225e8215ff855c694400fe60f7f
Anthony Grimes <i@raynes.me>
2011-06-24 18:06:48 -0500

    Add a split-id function to generalize.

--- src/ego/core.clj
+++ src/ego/core.clj
@@ -2,17 +2,20 @@
   (:use [useful :only [verify]]
         [clojure.string :only [join]]))
 
-(defn node-type [^String id]
-  (let [i (.indexOf id "-")]
-    (when (not= -1 i)
-      (.substring id 0 i))))
-
-(defn node-number [^String id & [node-type]]
+(defn split-id
+  "Split an id on -. If node-type is passed, verify that the type matches."
+  [^String id & [node-type]]
   (let [[type num] (.split id "-")]
     (verify (or (nil? node-type) (= node-type type))
             (format "node-id %s is not of type %s" id node-type))
-    (Long/parseLong num)))
+    [type (Long/parseLong num)]))
 
-(defn verify-type [expected id]
-  (verify (contains? expected (-> id node-type keyword))
-          (format "%s is not of type(s): %s" id (join ", " (map name expected)))))
+(defn node-type
+  "Get the type of an id. If node-type is passed, verify that the type matches."
+  [^String id & [node-type]]
+  (first (split-id id node-type)))
+
+(defn node-number
+  "Get the number of an id. If node-type is passed, verify that the type matches."
+  [^String id & [node-type]]
+  (second (split-id id node-type)))
