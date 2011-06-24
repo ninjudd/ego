e2b583215198b336bc694253b621c6251f4de31d
lance bradley <l@ncebradley.org>
2011-06-24 15:23:21 -0700

    add node-type node-number verify-type

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject ego "0.0.1-SNAPSHOT"
   :description "TODO: add summary of your project"
-  :dependencies [[clojure "1.2.0"]])
+  :dependencies [[clojure "1.2.1"]
+                 [clojure-useful "0.3.8"]])
--- src/ego/core.clj
+++ src/ego/core.clj
@@ -1,1 +1,18 @@
-(ns ego.core)
+(ns ego.core
+  (:use [useful :only [verify]]
+        [clojure.string :only [join]]))
+
+(defn node-type [^String id]
+  (let [i (.indexOf id "-")]
+    (when (not= -1 i)
+      (.substring id 0 i))))
+
+(defn node-number [^String id & [node-type]]
+  (let [[type num] (.split id "-")]
+    (verify (or (nil? node-type) (= node-type type))
+            (format "node-id %s is not of type %s" id node-type))
+    (Long/parseLong num)))
+
+(defn verify-type [expected id]
+  (verify (contains? expected (-> id node-type keyword))
+          (format "%s is not of type(s): %s" id (join ", " (map name expected)))))
