97f4d8fc8d850245265080c00b85e1c94330d4aa
Anthony Grimes <i@raynes.me>
2011-06-24 19:26:19 -0500

    Combine the functionality of the library into the single split-id function.

--- src/ego/core.clj
+++ src/ego/core.clj
@@ -3,19 +3,14 @@
         [clojure.string :only [join]]))
 
 (defn split-id
-  "Split an id on -. If node-type is passed, verify that the type matches."
-  [^String id & [node-type]]
-  (let [[type num] (.split id "-")]
-    (verify (or (nil? node-type) (= node-type type))
-            (format "node-id %s is not of type %s" id node-type))
+  "Split an id on -. Optionally pass a function (such as a set) that will be passed
+   the id's type. If this function returns false, an error will be thrown."
+  [^String id & [expected]]
+  (let [[type num] (.split id "-")
+        type (keyword type)]
+    (verify (or (nil? expected) (expected type))
+            (if (set? expected)
+              (format "node-id %s doesn't match type(s): %s"
+                      id (join ", " (map name expected)))
+              "node-id's type was not what was expected"))
     [type (Long/parseLong num)]))
-
-(defn node-type
-  "Get the type of an id. If node-type is passed, verify that the type matches."
-  [^String id & [node-type]]
-  (first (split-id id node-type)))
-
-(defn node-number
-  "Get the number of an id. If node-type is passed, verify that the type matches."
-  [^String id & [node-type]]
-  (second (split-id id node-type)))
