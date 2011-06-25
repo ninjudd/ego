c1eccfdc85c253b5a32a3caf3d96ca225f7b1d57
Anthony Grimes <i@raynes.me>
2011-06-24 19:33:50 -0500

    Add a readme and bump version number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ego "0.0.1-SNAPSHOT"
-  :description "TODO: add summary of your project"
+(defproject ego "0.1.0"
+  :description "A library for working with a simple 'id' format."
   :dependencies [[clojure "1.2.1"]
                  [clojure-useful "0.3.8"]])
