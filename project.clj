59a023b7eb487ca770807a02470396932bcda411
Anthony Grimes <i@raynes.me>
2011-07-19 16:39:19 -0500

    Added cake-marginalia to the deps.

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject ego "0.1.3"
+(defproject ego "0.1.1-SNAPSHOT"
   :description "A library for working with a simple 'id' format."
   :dependencies [[clojure "1.2.1"]
-                 [useful "0.5.0"]])
+                 [useful "0.4.0"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :tasks [cake-marginalia.tasks])
