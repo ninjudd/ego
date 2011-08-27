633dfad26ad40301698aef203ac84048bf09aa11
Justin Balthrop <git@justinbalthrop.com>
2011-08-26 18:54:49 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ego "0.1.4"
+(defproject ego "0.1.5"
   :description "A library for working with a simple 'id' format."
-  :dependencies [[clojure "1.2.1"]
-                 [useful "0.5.6"]]
-  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.2"]]
+  :dependencies [[clojure "1.2.0"]
+                 [useful "0.7.0-alpha3"]]
+  :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.3"]]
   :tasks [cake-marginalia.tasks])
