b1c336be8d653ce736fd32abbf3a4071aac5e11f
Alan Malloy <alan@malloys.org>
2011-09-26 18:16:04 -0700

    Update for useful

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ego "0.1.5"
+(defproject ego "0.1.6"
   :description "A library for working with a simple 'id' format."
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.0-alpha3"]]
+                 [useful "0.7.0-beta4"]]
   :dev-dependencies [[org.clojars.flatland/cake-marginalia "0.6.3"]]
   :tasks [cake-marginalia.tasks])
