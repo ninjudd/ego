2ab6fe108311dc0ca0b75ce55600140e6162c682
Anthony Grimes <i@raynes.me>
2011-12-14 16:40:25 -0600

    Update to 1.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ego "0.1.7"
+(defproject ego "0.1.8-alpha1"
   :description "A library for working with a simple 'id' format."
-  :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.0"]])
+  :dependencies [[clojure "1.3.0"]
+                 [useful "0.7.5"]])
