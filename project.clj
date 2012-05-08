ad794589c8bada85dc3dcfb2c7ad44e061ae9e4b
Justin Balthrop <git@justinbalthrop.com>
2012-05-07 17:59:34 -0700

    0.1.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ego "0.1.8-alpha1"
+(defproject ego "0.1.8"
   :description "A library for working with a simple 'id' format."
   :dependencies [[clojure "1.3.0"]
                  [useful "0.7.5"]])
