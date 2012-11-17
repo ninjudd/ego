5e9ea6d5edc33b6e766af29e6939ef10ee528cb7
Justin Balthrop <git@justinbalthrop.com>
2012-11-16 16:55:34 -0800

    0.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.flatland/ego "0.2.0-SNAPSHOT"
+(defproject org.flatland/ego "0.2.0"
   :url "https://github.com/flatland/ego"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "A library for working with a simple 'id' format."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.flatland/useful "0.8.9-SNAPSHOT"]]
+                 [org.flatland/useful "0.9.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
   :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
